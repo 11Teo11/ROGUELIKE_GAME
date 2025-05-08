2e36a3f05d2d807def46d84314d377b309db5e2c
Teodor <teov5058@gmail.com>
2025-05-08 22:50:17 +0300

    Update Game.cpp

--- JOC_ROGUELIKE/src/Game.cpp
+++ JOC_ROGUELIKE/src/Game.cpp
@@ -36,7 +36,11 @@ void Game::update(float dt)
         if (enemy)
         {
             if(enemy->isAlive())
-                enemy->moveTowardPlayer(player.getPosition(), map, dt);
+                {
+                    enemy->moveTowardPlayer(player.getPosition(), map, dt);
+                    enemy->shootPlayer(player.getPosition());
+                    enemy->updateProjectiles(dt, map, player);
+                }
             else
             {
                 delete(enemy);
@@ -50,9 +54,15 @@ void Game::update(float dt)
 void Game::draw(sf::RenderWindow& window) 
 {
     map.draw(window);
+    
     player.drawProjectiles(window);
+
     for(int i = 0; i < entities.size(); i++)
     {
+        Enemy* enemy = dynamic_cast<Enemy*>(entities[i]);
+        if(enemy)
+            enemy->drawProjectiles(window);
+            
         entities[i]->draw(window);
     }
     player.draw(window);
