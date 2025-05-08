bb6f08190c9b4c758550e0ab53e263b3112b223b
Teodor <teov5058@gmail.com>
2025-05-08 22:49:35 +0300

    Update Enemy.h

--- JOC_ROGUELIKE/src/Enemy.h
+++ JOC_ROGUELIKE/src/Enemy.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "Entity.h"
 #include "Map.h"
+#include "Projectile.h"
+#include "Player.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
+
+class Projectile;
 
 class Enemy : public Entity
 {
@@ -18,7 +22,12 @@ private:
     float frameDelay;
     int currentFrame;
     int health;
-    
+    sf::RectangleShape healthBarBack;
+    sf::RectangleShape healthBarFront;
+    std::vector<Projectile> projectiles;
+    float shootTimer;
+    float shootCooldown;
+
 public:
     Enemy(sf::Vector2f pos);
     Enemy* clone() const override;
@@ -27,10 +36,14 @@ public:
     void draw(sf::RenderWindow& window) override;
 
     sf::Vector2f getPosition() const override;
+    const std::vector<Projectile>& getProjectiles() const;
 
     void takeDamage(int dmg);
     bool isAlive() const;
 
     void moveTowardPlayer(const sf::Vector2f& playerPos, const Map& map, float dt);
     void randomMovement(float dt, const Map& map);
+    void shootPlayer(const sf::Vector2f& playerPos);
+    void updateProjectiles(float dt, const Map& map, Player& player);
+    void drawProjectiles(sf::RenderWindow& window);
 };
