9fb2a4699636d48cf0b8d615e8ad30a3ff892063
Teodor <teov5058@gmail.com>
2025-05-30 15:08:51 +0300

    Update Game.h

--- JOC_ROGUELIKE/src/Game.h
+++ JOC_ROGUELIKE/src/Game.h
@@ -10,13 +10,24 @@ private:
     Player player;
     std::vector<Entity*> entities;
 
+    Game(const Map& map, const Player& player);
+
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) =  delete;
 public:
-    Game(Map& map, Player& player);
+    static Game& getInstance(const Map& m, const Player& p){
+        static Game instance(m, p);
+        return instance;
+    }
 
     void update(float dt);
+
     void draw(sf::RenderWindow& window);
 
     void addEntity(Entity* e);
+
     const std::vector<Entity*>& getEntities() const;
     const Player& getPlayer() const;
+
+    void clearEntities();
 };
